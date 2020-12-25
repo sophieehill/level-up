6548ebd6daa5e2e6d97ae7c084c8f3dd7bffa987
sophieehill <43281937+sophieehill@users.noreply.github.com>
2020-12-24 22:12:45 -0500

    .

---
+++ content/home/members.R
@@ -0,0 +1,23 @@
+---
+widget: featurette
+headless: true  # This file represents a page section.
+title: 'Participating members'
+---
+
+
+# Showcase personal skills or business features.
+# Add/remove as many `feature` blocks below as you like.
+# For available icons, see: https://wowchemy.com/docs/page-builder/#icons
+feature:
+  - icon: r-project
+icon_pack: fab
+name: R
+description: 90
+- icon: chart-line
+icon_pack: fas
+name: Statistics
+description: 100
+- icon: camera-retro
+icon_pack: fas
+name: Photography
+description: 10
